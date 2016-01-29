8980ca6ea70ea3b0eb6b851c4e286f2f54ff13a5
alesr <alessandro.resta@gmail.com>
2016-01-29 12:15:58 +0000

    start sanitize input

--- msisdn/msisdn.go
+++ msisdn/msisdn.go
@@ -1,18 +1,32 @@
 package msisdn
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"strings"
+)
 
 type Msisdn struct {
 	input string
 }
 
 func (n *Msisdn) Decode(s string, reply *Response) error {
-	n.input = s
+
+	if err := n.sanitize(s); err != nil {
+		log.Fatal(err)
+	}
+
 	*reply = Response{"cc", "ndc", "mno"}
 	return nil
 }
 
-// func (n *Msisdn) sanitize(s *string)
+func (n *Msisdn) sanitize(s *string) error {
+	*s = strings.Replace(*s, " ", "", -1)
+	*s = strings.Replace(*s, "\t", "", -1)
+	*s = strings.TrimPrefix(*s, "+")
+	*s = strings.TrimPrefix(*s, "00")
+	return nil
+}
 
 // Response holds your data to be sent to client
 type Response struct {
